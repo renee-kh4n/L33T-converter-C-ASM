6b8b1fcd9e39d8b3f2472ba76cf12f3dea3a885a
renee-kh4n <renee.a.f.khan@gmail.com>
2024-12-21 21:37:50 +0800

    created the C function

--- main.c
+++ main.c
@@ -10,58 +10,85 @@ extern char asmfunc(char* c);
 
 void cFunc(char* strC) {
 
+	int i;
+	char c;
+	for (i = 0; i < strlen(strC); i++) {
+		c = strC[i];
+
+		if (c == 'a' || c == 'A') {
+			strC[i] = '4';
+		}
+		else if (c == 'e' || c == 'E') {
+			strC[i] = '3';
+		}
+		else if (c == 'i' || c == 'I') {
+			strC[i] = '!';
+		}
+		else if (c == 'o' || c == 'O') {
+			strC[i] = '0';
+		}
+		else if (c == 'u' || c == 'U') {
+			strC[i] = 'V';
+		}
+
+	}
+
 	
 	
 
 }
 
-void init(char* strC) {
+void init(char* str) {
 	int i;
 	for (i = 0; i < MAX; i += 10) {
-		strC[i] = 0;
-		strC[i + 1] = 0;
-		strC[i + 2] = 0;
-		strC[i + 3] = 0;
-		strC[i + 4] = 0;
-		strC[i + 5] = 0;
-		strC[i + 6] = 0;
-		strC[i + 7] = 0;
-		strC[i + 8] = 0;
-		strC[i + 9] = 0;
+		str[i] = 0;
+		str[i + 1] = 0;
+		str[i + 2] = 0;
+		str[i + 3] = 0;
+		str[i + 4] = 0;
+		str[i + 5] = 0;
+		str[i + 6] = 0;
+		str[i + 7] = 0;
+		str[i + 8] = 0;
+		str[i + 9] = 0;
 	}
 }
 
 int main() {
 
 	clock_t start, end;
-	char strC[MAX], strAsm[MAX];
+	char str[MAX], strC[MAX], strAsm[MAX];
 	double cTime = 0, asmTime = 0;
 
+	//initialize strings
+	init(str);
 	init(strC);
 	init(strAsm);
 
+	//prompt the user
 	printf("L33T Converter\n\n");
 
 	printf("Enter a string: ");
 	scanf_s("%199s", strC, (unsigned)_countof(strC)); // use scanf_s to scan the string
 
-	strcpy_s(strAsm, sizeof(strAsm), strC);      // use strcpy_s to copy the string
+	strcpy_s(strAsm, sizeof(strAsm), strC);			  // use strcpy_s to copy the string
 	printf("Original String: %s \n\n", strAsm);
 
-	printf("---------------C function---------------\n");
+	//call the C function
+	printf("---------------C function ---------------\n");
 	start = clock();
-	//cFunc(strC);
-	asmfunc(strC);
+	cFunc(strC);
 	end = clock();
-	cTime = (double)(end - start) * 10000 / CLOCKS_PER_SEC;
+	cTime = (double)(end - start) * 100000 / CLOCKS_PER_SEC;
 	printf("C Output: %s\n", strC);
 	printf("C function time: %f ms\n\n", cTime);
 
+	//call the ASM function
 	printf("-------------ASM function---------------\n");
 	start = clock();
 	asmfunc(strAsm);
 	end = clock();
-	asmTime = (double)(end - start) * 10000 / CLOCKS_PER_SEC;
+	asmTime = (double)(end - start) * 100000 / CLOCKS_PER_SEC;
 	printf("ASM Output: %s\n", strAsm);
 	printf("ASM function time: %f ms\n\n", asmTime);
 
